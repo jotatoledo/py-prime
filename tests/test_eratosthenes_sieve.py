4f412a862855abc67e7014e1700c39532d665545
Jose Toledo <jotatoledon@gmail.com>
2019-01-03 15:35:33 +0100

    test: add test suite for erastosthenes sieve

---
+++ tests/test_eratosthenes_sieve.py
@@ -0,0 +1,22 @@
+import pytest
+
+from context import pysieves
+from pysieves import erastothenes_sieve
+
+
+def test_Throws_When_GivenNegativeNumber():
+    with pytest.raises(ValueError):
+        erastothenes_sieve(-1)
+
+
+# TODO: move to external file
+@pytest.mark.parametrize("test_input,expected", [
+    (2, [2]),
+    (5, [2, 3, 5]),
+    (50, [2,
+          3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]),
+])
+def test_Correctly_Works(test_input: int, expected: [int]):
+    result = list(erastothenes_sieve(test_input))
+
+    assert result == expected
